1e1fe39d10727a3a78d4dc9c13778b3c888eeba6
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-08 15:37:04 +0200

    Attempt to explore security

--- src/main/java/be/technifutur/demo_mvc/securities/WebSecurityConfig.java
+++ src/main/java/be/technifutur/demo_mvc/securities/WebSecurityConfig.java
@@ -1,15 +1,27 @@
 package be.technifutur.demo_mvc.securities;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        jsr250Enabled = true, // Vérifie un rôle
-        securedEnabled = true, // Vérifie un rôle
-        prePostEnabled = true // Vérifie bien plus de choses
-)
-public class WebSecurityConfig {
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(
+//        jsr250Enabled = true, // Vérifie un rôle
+//        securedEnabled = true, // Vérifie un rôle
+//        prePostEnabled = true // Vérifie bien plus de choses
+//)
+public class WebSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic();
+//
+//        http.authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/product/**").permitAll()
+//                .anyRequest().authenticated();
+//    }
 }
